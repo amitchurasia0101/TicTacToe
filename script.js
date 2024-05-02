e27ee9ba12ef0debad548b3bfc83a25527f4872b
amitchurasia0101 <145024651+amitchurasia0101@users.noreply.github.com>
2024-05-02 13:38:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,21 +25,25 @@ const resetGame = () => {
 }
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () => { 
+    box.addEventListener("click", () => {
+        const audioX = new Audio('tap.mp3');
+        const audioO = new Audio('sound_o.mp3');
         if(turnO){
             box.innerText = "O";
             box.style.color = "#fff"; 
             box.style.textShadow = "0 0 10px red, 0 0 20px red, 0 0 30px red, 0 0 40px #ff7c7c, 0 0 70px #ff7c7c, 0 0 80px #ff7c7c, 0 0 100px #ff7c7c, 0 0 150px #ff7c7c"
-            
+            audioO.play(); 
             turnO = false;
         }else{
             box.innerText = "X";
             box.style.color = "#fff"; 
-            box.style.textShadow = " 0 0 10px blue, 0 0 20px blue, 0 0 30px blue, 0 0 40px #75aaff, 0 0 70px #75aaff, 0 0 80px #75aaff, 0 0 100px #75aaff, 0 0 150px #75aaff"
+            box.style.textShadow = " 0 0 10px blue, 0 0 20px blue, 0 0 30px blue, 0 0 40px #75aaff, 0 0 70px #75aaff, 0 0 80px #75aaff, 0 0 100px #75aaff, 0 0 150px #75aaff";
+            audioX.play();
             turnO = true;
         }
         box.disabled = true;
         checkWinner();
+        checkDraw();
      });
 });
 
@@ -64,11 +68,22 @@ const enableBoxes = () => {
  }
 
 const showWinner = (winner) => { 
+    const audiowinner = new Audio('congo.mp3');
+    audiowinner.play(); 
     msg.innerText = `Congratulations, Winner is ${winner} `;
     msgContainer.classList.remove("hide");
     disableBoxes();
  };
+ 
+ const showDraw = () => { 
+    const audiodraw = new Audio('shit.mp3');
+    audiodraw.play(); 
+    msg.innerText = `Ahh, It's a Draw!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
 
+ 
 const checkWinner = () => { 
     for (let patterns of winPatterns) {
         let pos1Val = boxes[patterns[0]].innerText;
@@ -82,8 +97,27 @@ const checkWinner = () => {
                 showWinner(pos1Val);
             }
         }
+        
     }
  };
+ const checkDraw = () => { 
+    let isDraw = true;
+    for (let patterns of winPatterns) {
+        let pos1Val = boxes[patterns[0]].innerText;
+        let pos2Val = boxes[patterns[1]].innerText;
+        let pos3Val = boxes[patterns[2]].innerText;
+
+        if (pos1Val === "" || pos2Val === "" || pos3Val === "") {
+            isDraw = false;
+            break; // If any box is empty, it's not a draw
+        }
+    }
+    if (isDraw) {
+        showDraw();
+    }
+};
+
+
 
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
